c91a7d4fa1c25c045be921e0784337525cc3ed51
Ben Muschol <benjamin.muschol@airtable.com>
2023-09-04 14:29:32 -0400

    Add guess state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import "./App.css";
 
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import { useState } from "react";
+import { Map } from "react-map-gl";
+
+const initialViewState = {
+  longitude: -71.0593,
+  latitude: 42.35,
+  zoom: 10,
+};
+
+function GameplayMap() {
+  const [guess, setGuess] = useState(null);
+  return (
+    <Map
+      style={{ width: 600, height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      initialViewState={initialViewState}
+      maxZoom={12}
+      minZoom={8.5}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>We have a blank canvas!</h1>
+      <GameplayMap />
     </div>
   );
 }
