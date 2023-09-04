f08cbe7a74d4ede42d56a6a150689c7f542e30b9
Ben Muschol <benjamin.muschol@airtable.com>
2023-09-04 14:39:16 -0400

    Display and calculate guess score

--- src/App.js
+++ src/App.js
@@ -14,6 +14,37 @@ const initialViewState = {
   zoom: 10,
 };
 
+// calculate distance in meters using https://www.movable-type.co.uk/scripts/latlong.html
+function distanceInMeters(a, b) {
+  const lng1 = a.longitude;
+  const lng2 = b.longitude;
+  const lat1 = a.latitude;
+  const lat2 = b.latitude;
+
+  const a1 = (lat1 * Math.PI) / 180;
+  const a2 = (lat2 * Math.PI) / 180;
+
+  const deltaA1A2 = ((lat2 - lat1) * Math.PI) / 180;
+  const deltaLambda = ((lng2 - lng1) * Math.PI) / 180;
+
+  const res =
+    Math.sin(deltaA1A2 / 2) ** 2 +
+    Math.cos(a1) * Math.cos(a2) * Math.sin(deltaLambda / 2) ** 2;
+  const c = 2 * Math.atan2(Math.sqrt(res), Math.sqrt(1 - res));
+  return 6371e3 * c;
+}
+
+/**
+ * Calculate the score of a guess with the maximum score of 5,000
+ * Scoring uses a quadratic function to reward being very close. At 15,000 meters
+ * away, the score becomes 0
+ */
+export function getScore(guess, actualLocation) {
+  const distance = distanceInMeters(guess, actualLocation);
+
+  return Math.round((5000 / 15000 ** 2) * Math.max(15000 - distance, 0) ** 2);
+}
+
 function GameplayMap() {
   const [guess, setGuess] = useState(null);
   const [isGuessConfirmed, setIsGuessConfirmed] = useState(false);
@@ -28,6 +59,7 @@ function GameplayMap() {
 
   let confirmGuessButton = null;
   let stationMarker = null;
+  let scoreDisplay = null;
   if (isGuessConfirmed) {
     // only show the station once the user confirms their guess
     stationMarker = (
@@ -37,6 +69,7 @@ function GameplayMap() {
         color="red"
       />
     );
+    scoreDisplay = <p>Score: {getScore(guess, station)}</p>;
   } else {
     confirmGuessButton = (
       <button
@@ -73,6 +106,7 @@ function GameplayMap() {
       </Map>
 
       {confirmGuessButton}
+      {scoreDisplay}
     </div>
   );
 }
